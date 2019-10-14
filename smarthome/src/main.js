0570c15d5cd5dee6bc6a35f86da0ed4e5187c9ed
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-10-14 23:28:34 +0700

    added debounce

--- smarthome/src/main.js
+++ smarthome/src/main.js
@@ -4,12 +4,33 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import VueAWN from "vue-awesome-notifications"
+import "vue-awesome-notifications/dist/styles/style.css"
+
+
+Vue.use(VueAWN, {
+    durations: {
+        global: 2000
+    }
+})
 
 Vue.config.productionTip = false
 
+Vue.mixin({
+    methods: {
+        next: function ({ response: { data: { code, message } } }) {
+            // next: function (obj) {
+            const vm = this
+
+            if (Array.isArray(message)) message = message.join('\n')
+            vm.$awn.alert(message, { labels: { alert: `Error ${code}` } })
+        },
+    }
+})
+
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
