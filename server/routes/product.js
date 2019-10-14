c16d0bc7b30ca20da78bc572a12b05c810897adb
Ricky <audrickong.ao@gmail.com>
2019-10-14 22:15:51 +0700

    Final Fix

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const productController = require('../controllers/product')
+const { authorization, authentication } = require("../middlewares/auth")
 
 router.get('/', productController.findAll)
 router.get('/:id', productController.findOne)
+router.use(authentication, authorization)
 router.post('/', productController.create)
 router.patch('/:id', productController.update)
 router.delete('/:id', productController.delete)
