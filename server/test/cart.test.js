a4e0a9b79115f62768c20e21a86a6ef716fed4b3
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-10-16 13:00:48 +0700

    up

--- server/test/cart.test.js
+++ server/test/cart.test.js
@@ -2,7 +2,7 @@
 const chai = require('chai')
 const chaiHTTP = require('chai-http')
 const expect = chai.expect
-const fs = require('fs')
+// const fs = require('fs')
 const app = require('../app')
 const Product = require('../models/product')
 const User = require('../models/user')
@@ -13,11 +13,12 @@ let userToken = null
 
 let productId1 = "5da6965e8e68ae10fa3c6125"
 let productId2 = "5da6969e1be6ec129d832c6e"
+let cartAmount = 0
 
 before(function (done) {
     const user = {
-        username: "tigor",
-        email: "tigor@email.com",
+        username: "tigor123",
+        email: "tigor123@email.com",
         password: "12345"
     }
 
@@ -33,9 +34,9 @@ before(function (done) {
                 })
         })
     }
-    const productSeed = JSON.parse(fs.readFileSync('./test/seed/products.json'))
+    // const productSeed = JSON.parse(fs.readFileSync('./test/seed/products.json'))
 
-    Promise.all([getUsertoken(), Product.insertMany(productSeed)])
+    Promise.all([getUsertoken()])
         .then(() => done())
 })
 
@@ -60,6 +61,7 @@ describe('Cart Route', function () {
                         expect(el).to.have.all.keys('proudct', 'quantity')
                         expect(el.product).to.have.all.keys('_id', 'productName', 'image', 'price', 'stock', 'createdAt', 'updatedAt')
                         expect(el.quantity).to.be.a('number')
+                        cartAmount++
                     })
                     done()
                 })
@@ -95,17 +97,21 @@ describe('Cart Route', function () {
                 .send(data)
                 .set({ Authorization: userToken })
                 .end(function (err, res) {
+                    let countCart = 0
                     expect(err).to.be.null
                     expect(res).to.have.status(200)
                     expect(res.body).to.have.property('cart')
                     expect(res.body.cart).to.be.an('array')
-                    expect(res.body.cart).to.have.lengthOf(1)
+                    expect(res.body.cart).to.have.lengthOf.at.least(1)
                     res.body.cart.forEach(el => {
                         expect(el).to.be.an('object')
                         expect(el).to.have.all.keys('proudct', 'quantity')
                         expect(el.product).to.have.all.keys('_id', 'productName', 'image', 'price', 'stock', 'createdAt', 'updatedAt')
                         expect(el.quantity).to.be.a('number')
+                        countCart++
                     })
+                    expect(countCart).to.be.greaterThan(cartAmount)
+                    cartAmount = countCart
                     done()
                 })
         })
@@ -202,6 +208,64 @@ describe('Cart Route', function () {
                 .post('/carts')
                 .send(data)
                 .set({ Authorization: userToken })
+                .end(function (err, res) {
+                    expect(err).to.be.null;
+                    expect(res).to.have.status(401)
+                    expect(res.body).to.be.an('object')
+                    expect(res.body).to.have.all.keys('code', 'message')
+                    const { code, message } = res.body
+                    expect(code).to.equal(res.status)
+                    expect(message).to.be.a('string')
+                    expect(message).to.equal("Token not set for this request")
+                    done()
+                })
+        })
+    })
+
+    describe('Remove item from cart', function () {
+        it('Success remove item from cart', function (done) {
+            chai.request(app)
+                .patch('/carts/' + productId1)
+                .set({ Authorization: userToken })
+                .end(function (err, res) {
+                    let countCart = 0
+                    expect(err).to.be.null
+                    expect(res).to.have.status(200)
+                    expect(res.body).to.have.property('cart')
+                    expect(res.body.cart).to.be.an('array')
+                    res.body.cart.forEach(el => {
+                        expect(el).to.be.an('object')
+                        expect(el).to.have.all.keys('proudct', 'quantity')
+                        expect(el.product).to.have.all.keys('_id', 'productName', 'image', 'price', 'stock', 'createdAt', 'updatedAt')
+                        expect(el.quantity).to.be.a('number')
+                        countCart++
+                    })
+                    expect(countCart).to.be.lessThan(cartAmount)
+                    cartAmount = countCart
+                    done()
+                })
+
+        })
+
+        it('Fail Add Product to Cart - Product Not Found', function (done) {
+            chai.request(app)
+                .patch('/carts/' + productId1)
+                .set({ Authorization: userToken })
+                .end(function (err, res) {
+                    expect(err).to.be.null;
+                    expect(res).to.have.status(404)
+                    expect(res.body).to.be.an('object')
+                    expect(res.body).to.have.all.keys('code', 'message')
+                    const { code, message } = res.body
+                    expect(code).to.equal(res.status)
+                    expect(message).to.be.a('string')
+                    expect(message).to.equal("There's no product with such id in your cart")
+                    done()
+                })
+        })
+        it('Fail Add Product to Cart - Token Not Set', function (done) {
+            chai.request(app)
+                .patch('/carts/' + productId1)
                 .end(function (err, res) {
                     expect(err).to.be.null;
                     expect(res).to.have.status(401)
