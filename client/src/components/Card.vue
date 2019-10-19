72f6ddbeebd51ff180f14680b6389143bced7afb
dipadana <putudipadana@gmail.com>
2019-10-19 14:56:08 +0700

    Vue cli init

---
+++ client/src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-card
+      :title="title"
+      :img-src="imgsrc"
+      img-alt="Image"
+      img-top
+      tag="article"
+      class="mb-2"
+    >
+      <b-card-text>
+        {{descrip || "this is nice product"}}
+      </b-card-text>
+      <b-button href="#" variant="primary">Detail</b-button>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  data () {
+    return {
+
+    }
+  },
+  props: {
+    imgsrc: String,
+    title: String,
+    subtitle: String,
+    descrip: String
+  }
+}
+</script>
+
+<style>
+
+</style>
